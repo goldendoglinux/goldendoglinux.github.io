d59eb42fa033bfcbb98522a0dfbb9d8433fa6c33
Alexia Michelle <alexia@goldendoglinux.org>
2025-12-26 17:39:40 -0300

    woot

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -37,7 +37,7 @@ const FeatureList = [
 
 function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col col--4', styles.featureCard)}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
