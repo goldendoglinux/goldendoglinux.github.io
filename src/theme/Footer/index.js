bf7c85190a291155b5349ab0c4f30418bfca3122
Alexia Michelle <alexia@lexiworks.dev>
2026-05-12 11:50:04 -0300

    shurg host banner

---
+++ src/theme/Footer/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Footer from '@theme-original/Footer';
+import ThemedImage from '@theme/ThemedImage';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import Link from '@docusaurus/Link';
+
+export default function FooterWrapper(props) {
+  return (
+    <>
+      <div className="sponsor-banner-wrapper">
+        <Link
+          href="https://shurg.host"
+          className="sponsor-banner-link"
+          aria-label="Powered by Shrug.host"
+        >
+          <ThemedImage
+            alt="Powered by Shrug.host"
+            className="sponsor-banner-img"
+            sources={{
+              light: useBaseUrl('/img/shrug-host-dark.svg'),
+              dark: useBaseUrl('/img/shrug-host-light.svg'),
+            }}
+          />
+        </Link>
+      </div>
+      <Footer {...props} />
+    </>
+  );
+}
